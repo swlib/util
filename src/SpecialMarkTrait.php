a222d0013d93cf8727cbd8e8c47d10e9876da041
twosee <twose@qq.com>
2018-04-12 22:16:16 +0800

    Add special mark trait.

---
+++ src/SpecialMarkTrait.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Copyright: Swlib
+ * Author: Twosee <twose@qq.com>
+ * Date: 2018/4/12 下午9:48
+ */
+
+namespace Swlib\Util;
+
+trait SpecialMarkTrait
+{
+
+    /** @var $special_marks array "mark/remark" */
+    public $special_marks = ['default' => null];
+
+    /**
+     * Get special mark of this object
+     *
+     * @param string $name
+     * @return mixed
+     */
+    public function getSpecialMark(string $name = 'default')
+    {
+        return $this->special_marks[$name] ?? null;
+    }
+
+    /**
+     * Mark this object
+     *
+     * @param mixed $mark
+     * @return $this
+     */
+    public function withSpecialMark($mark, string $name = 'default'): self
+    {
+        $this->special_marks[$name] = $mark;
+
+        return $this;
+    }
+
+}
