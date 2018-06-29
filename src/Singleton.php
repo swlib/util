fda696b7704c64fd294335e0e356b9859be89719
twosee <twose@qq.com>
2018-06-29 23:31:18 +0800

    Add Singleton trait.

---
+++ src/Singleton.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Author: Twosee <twose@qq.com>
+ * Date: 2018/6/29 下午11:29
+ */
+
+namespace Swlib\Util;
+
+trait Singleton
+{
+    private static $instance;
+
+    static function getInstance(...$args)
+    {
+        if (!isset(self::$instance)) {
+            /** @noinspection PhpMethodParametersCountMismatchInspection */
+            self::$instance = new static(...$args);
+        }
+        return self::$instance;
+    }
+
+}
