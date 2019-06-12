b6c00b213111781c2cf7111bab1eae1e26b5ddae
twosee <twose@qq.com>
2019-06-12 20:51:45 +0800

    Improve TypeDetector.

--- src/TypeDetector.php
+++ src/TypeDetector.php
@@ -12,10 +12,9 @@ use Iterator;
 
 class TypeDetector
 {
-
-    public static function canBeArray($var): bool
+    public static function isIterable($var): bool
     {
-        return is_array($var) || (is_object($var) && $var instanceof ArrayAccess);
+        return is_array($var) || is_iterable($var) || (is_object($var) && $var instanceof Iterator);
     }
 
     public static function canBeString($var): bool
@@ -23,14 +22,22 @@ class TypeDetector
         return is_string($var) || (is_object($var) && method_exists($var, '__toString'));
     }
 
+    public static function canBeArray($var): bool
+    {
+        return is_array($var) || (is_object($var) && $var instanceof ArrayAccess);
+    }
+
     public static function canBeCalled($var): bool
     {
-        return is_callable($var) || (is_object($var) && method_exists($var, '__invoke'));
+        if (is_callable($var)) {
+            return true;
+        }
+        if (is_array($var) && count($var) === 2) {
+            return @method_exists(...$var);
+        }
+        if (is_object($var) && method_exists($var, '__invoke')) {
+            return true;
+        }
+        return false;
     }
-
-    public static function isIterable($var): bool
-    {
-        return is_array($var) || (is_object($var) && $var instanceof Iterator);
-    }
-
 }
